885fcbad330d8860d84d9c173e210d2f54ffb234
Cristian Ion <cristian.ion94@gmail.com>
2023-08-25 15:24:58 +0300

    nb

--- solution/dstl_detection/dstl_processing.py
+++ solution/dstl_detection/dstl_processing.py
@@ -17,34 +17,38 @@ def read_train_wkt(train_wkt_csv=TRAIN_WKT_FILE):
 
 
 class DstlProcessingLib:
-    def __init__(self, df_train_wkt, df_grid_sizes) -> None:
+    def __init__(self, df_train_wkt, df_grid_sizes, classes) -> None:
         self.df_train_wkt = df_train_wkt
         self.df_grid_sizes =  df_grid_sizes
-        self.num_classes = 10
+        self.classes = classes
 
     def read_image_and_mask(self, raster_size, image_id):
         img_path = os.path.join(THREE_BAND, image_id + EXT_TIFF)
         img = tiff.imread(img_path)
 
-        masks = [self.generate_mask_for_image_and_class(raster_size, image_id, i) for i in range(len(self.num_classes))]
+        if img.shape[0] == 3:
+            img = np.rollaxis(img, 0, 3)
+
+        masks = [self.generate_mask_for_image_and_class(raster_size, image_id, i) for i in range(len(self.classes))]
 
         return img, masks
 
     def generate_mask_for_image_and_class(self, raster_size, image_id, class_type):
         # __author__ = visoft
         # https://www.kaggle.com/visoft/dstl-satellite-imagery-feature-detection/export-pixel-wise-mask
-        xymax = self._get_xmax_ymin(self.df_grid_sizes, image_id)
-        polygon_list = self._get_polygon_list(self.df_train_wkt, image_id, class_type)
+        xymax = self._get_xmax_ymin(image_id)
+        polygon_list = self._get_polygon_list(image_id, class_type)
         contours = self._get_and_convert_contours(polygon_list, raster_size, xymax)
         mask = self._plot_mask_from_contours(raster_size, contours, 1)
         return mask
 
-    def _get_xmax_ymin(self, grid_sizes_df, imageId: int):
+    def _get_xmax_ymin(self, imageId: int):
         # __author__ = visoft
         # https://www.kaggle.com/visoft/dstl-satellite-imagery-feature-detection/export-pixel-wise-mask
-        xmax, ymin = grid_sizes_df[grid_sizes_df.ImageId == imageId].iloc[0, 1:].astype(float)
+        xmax, ymin = self.df_grid_sizes[self.df_grid_sizes.ImageId == imageId].iloc[0, 1:].astype(float)
         return (xmax, ymin)
 
+    @staticmethod
     def _convert_coordinates_to_raster(coords, img_size, xymax):
         # __author__ = visoft
         # https://www.kaggle.com/visoft/dstl-satellite-imagery-feature-detection/export-pixel-wise-mask
@@ -59,23 +63,23 @@ class DstlProcessingLib:
         coords_int = np.round(coords).astype(np.int32)
         return coords_int
 
-    def _get_and_convert_contours(self, polygon_list, raster_img_size, xymax):
+    def _get_and_convert_contours(self, multipolygon, raster_img_size, xymax):
         # __author__ = visoft
         # https://www.kaggle.com/visoft/dstl-satellite-imagery-feature-detection/export-pixel-wise-mask
-        perim_list = []
-        interior_list = []
-        if polygon_list is None:
+        raster_coords_list = []
+        raster_interior_list = []
+        if multipolygon is None:
             return None
-        for k in range(len(polygon_list)):
-            poly = polygon_list[k]
-            perim = np.array(list(poly.exterior.coords))
-            perim_c = self._convert_coordinates_to_raster(perim, raster_img_size, xymax)
-            perim_list.append(perim_c)
+
+        for poly in multipolygon.geoms:
+            vector_coords = np.array(list(poly.exterior.coords))
+            raster_coords = DstlProcessingLib._convert_coordinates_to_raster(vector_coords, raster_img_size, xymax)
+            raster_coords_list.append(raster_coords)
             for pi in poly.interiors:
                 interior = np.array(list(pi.coords))
-                interior_c = self._convert_coordinates_to_raster(interior, raster_img_size, xymax)
-                interior_list.append(interior_c)
-        return perim_list, interior_list
+                interior_c = DstlProcessingLib._convert_coordinates_to_raster(interior, raster_img_size, xymax)
+                raster_interior_list.append(interior_c)
+        return raster_coords_list, raster_interior_list
 
     def _plot_mask_from_contours(self, raster_size, contours, class_value=1):
         # __author__ = visoft
@@ -94,11 +98,11 @@ class DstlProcessingLib:
         # https://www.kaggle.com/visoft/dstl-satellite-imagery-feature-detection/export-pixel-wise-mask
         df_image = self.df_train_wkt[self.df_train_wkt.ImageId == image_id]
         multipoly_def = df_image[df_image.ClassType == class_type].MultipolygonWKT
-        polygonList = None
+        polygons = None
         if len(multipoly_def) > 0:
             assert len(multipoly_def) == 1
-            polygonList = wkt_loads(multipoly_def.values[0])
-        return polygonList
+            polygons = wkt_loads(multipoly_def.values[0])
+        return polygons
 
 
 if __name__ == "__main__":
