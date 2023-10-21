da48c6357ca1b175fdd9f5babe4476b115f5001f
Cristian Ion <cristian.ion94@gmail.com>
2023-10-21 20:29:03 +0300

    asds

---
+++ test/inria/evaluate_inria.py
@@ -0,0 +1,5 @@
+# todo: - class-confusion matrix (use gray as color map, expect light diagonal )
+# todo: - accuracy
+# todo: - IoU (predict == label / )
+# todo: - ROC curve - pg. 501, Computer Vision: A modern approach
+# todo: - Precision as a function of recall - pg. 507, Computer Vision: A modern approach
