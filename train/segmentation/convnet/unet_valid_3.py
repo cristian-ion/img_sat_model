585eae9696303a3c6482dbe27324fc03462eefcc
Cristian Ion <cristian.ion94@gmail.com>
2023-11-07 22:47:15 +0200

    unet valid 3

---
+++ train/segmentation/convnet/unet_valid_3.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+CONV_PAD = 'valid' # was 1
+BIAS = False
+
+
+class SingleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1, padding=0):
+        super().__init__()
+        self.single_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=BIAS),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.single_conv(x)
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=CONV_PAD, bias=BIAS),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=CONV_PAD, bias=BIAS),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class DoubleConvResidual(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+
+        self.conv1 = SingleConv(in_channels, mid_channels, kernel_size=3, padding=CONV_PAD)
+        self.conv2 = SingleConv(in_channels, out_channels, kernel_size=3, padding=CONV_PAD)
+
+    def forward(self, x_top):
+        x = self.conv1(x_top)
+        x = self.conv2(x)
+        return x + x_top
+
+
+class DownConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2), DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class UpConv(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+            self.conv_1x1_half_channels = SingleConv(in_channels, in_channels // 2)
+            self.conv_1x1 = SingleConv(in_channels, in_channels)
+        else:
+            self.upsample = nn.ConvTranspose2d(
+                in_channels, in_channels // 2, kernel_size=2, stride=2
+            )
+        self.double_conv_3x3 = DoubleConv(in_channels, out_channels)
+
+    def forward(self, decoder_layer, encoder_layer):
+        encoder_layer = self.adjust_encoder_layer(encoder_layer)
+        decoder_layer = self.adjust_decoder_layer(decoder_layer, encoder_layer)
+        x = torch.cat([encoder_layer, decoder_layer], dim=1)
+        x = self.double_conv_3x3(x)
+        return x
+
+    def adjust_decoder_layer(self, decoder_layer, encoder_layer):
+        decoder_layer = self.conv_1x1_half_channels(decoder_layer)
+        decoder_layer = self.upsample(decoder_layer)
+        diffY = (encoder_layer.size()[2] - decoder_layer.size()[2]) // 2
+        diffX = (encoder_layer.size()[3] - decoder_layer.size()[3]) // 2
+        decoder_layer = F.pad(decoder_layer, [diffX, diffX, diffY, diffY])
+        return decoder_layer
+
+    def adjust_encoder_layer(self, encoder_layer):
+        encoder_layer = self.conv_1x1(encoder_layer)
+        return encoder_layer
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNetValid3(nn.Module):
+    def __init__(self, in_channels, n_classes, bilinear=False):
+        super(UNetValid3, self).__init__()
+        self.n_channels = in_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.conv0 = DoubleConv(in_channels, 64)
+        self.down_conv1 = DownConv(64, 128)
+        self.down_conv2 = DownConv(128, 256)
+        self.down_conv3 = DownConv(256, 512)
+        self.down_conv4 = DownConv(512, 1024)
+
+        self.up_conv1 = UpConv(1024, 512, bilinear)
+        self.up_conv2 = UpConv(512, 256, bilinear)
+        self.up_conv3 = UpConv(256, 128, bilinear)
+        self.up_conv4 = UpConv(128, 64, bilinear)
+        self.out_conv = OutConv(64, n_classes)
+
+    def forward(self, x):
+        # encoder section
+        x1 = self.conv0(x)
+        x2 = self.down_conv1(x1)
+        x3 = self.down_conv2(x2)
+        x4 = self.down_conv3(x3)
+        x5 = self.down_conv4(x4)
+
+        # decoder section
+        x = self.up_conv1(x5, x4)
+        x = self.up_conv2(x, x3)
+        x = self.up_conv3(x, x2)
+        x = self.up_conv4(x, x1)
+        logits = self.out_conv(x)
+        return logits
+
+
+    def use_checkpointing(self):
+        self.conv0 = torch.utils.checkpoint(self.conv0)
+        self.down_conv1 = torch.utils.checkpoint(self.down_conv1)
+        self.down_conv2 = torch.utils.checkpoint(self.down_conv2)
+        self.down_conv3 = torch.utils.checkpoint(self.down_conv3)
+        self.down_conv4 = torch.utils.checkpoint(self.down_conv4)
+        self.up_conv1 = torch.utils.checkpoint(self.up_conv1)
+        self.up_conv2 = torch.utils.checkpoint(self.up_conv2)
+        self.up_conv3 = torch.utils.checkpoint(self.up_conv3)
+        self.up_conv4 = torch.utils.checkpoint(self.up_conv4)
+        self.out_conv = torch.utils.checkpoint(self.out_conv)
+
+
+if __name__ == "__main__":
+    x = torch.randn((1, 3, 572, 572))
+    model = UNetValid3(in_channels=3, n_classes=1, bilinear=True)
+    preds = model(x)
+
+
+# Changelog
+# - Added residual connections in double conv residual
+# - added 1x1 conv on the encoder layer before concat with the upscaled decoder layer
